e716a93367a9f711818293c6e53d38b435786327
Marton Pentek <pentek.marci@gmail.com>
2022-10-07 10:23:44 +0200

    initial java backend

---
+++ Backend service/AutoGatrorBackend/src/main/java/com/autogator/autogatrorbackend/AutoGatrorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autogator.autogatrorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGatrorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoGatrorBackendApplication.class, args);
+    }
+
+}
---
+++ Backend service/AutoGatrorBackend/src/test/java/com/autogator/autogatrorbackend/AutoGatrorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autogator.autogatrorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGatrorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
