a620e60210d9e545e973fd7f71b2ea5c6b490f07
runnerm <pentek.marciggmail.com>
2022-10-13 14:20:12 +0200

    create autogator package and add networking node initial code

---
+++ Machine system/machine_ws/src/autogator/src/networking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import http.client
+
+def client():
+    rospy.init_node('networking', anonymous=True)
+    rate = rospy.Rate(0.2) # 1hz
+    while not rospy.is_shutdown():
+        connection = http.client.HTTPSConnection("autogator-servcie.herokuapp.com")
+        connection.request("GET", "/ros")
+        response = connection.getresponse()
+        hello_str = response.read().decode()
+        rospy.loginfo(hello_str)
+        rate.sleep()
+        connection.close()
+
+
+if __name__ == '__main__':
+    try:
+        client()
+    except rospy.ROSInterruptException:
+        pass
