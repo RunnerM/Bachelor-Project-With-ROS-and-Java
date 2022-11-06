f24b5ed7740eb842b18cbd103afc7d838dcd3407
runnerking <293649@viauc.dk>
2022-11-06 14:04:33 +0100

    wip with worker and messages

--- Machine system/machine_ws/src/autogator/nodes/networking.py
+++ Machine system/machine_ws/src/autogator/nodes/networking.py
@@ -9,13 +9,15 @@ from autogator.utils.autogator_worker import AutogatorWorker
 
 class networking:
     def __init__(self):
-        rospy.init_node('networking', anonymous=True)
+        rospy.init_node('networking')
         rospy.Subscriber("gps_track", gps_track, NetworkingService.upload_track)  # from master
         rospy.Subscriber("machine_state", machine_state, NetworkingService.upload_machinestate)  # from master
         rospy.Subscriber("location", location, NetworkingService.upload_location)  # from gps
-        # Starting worker thread so it doesn't block the main thread
-        AutogatorWorker("networking", NetworkingService.scan_command()).start()
         rospy.loginfo("Networking started.")
+        # Starting worker thread so it doesn't block the main thread
+        scan_command = AutogatorWorker("scan_command", function=NetworkingService.scan_command())
+        scan_command.start()
+        # threading.Thread(target=NetworkingService.scan_command()).start()
 
         rospy.spin()
 
