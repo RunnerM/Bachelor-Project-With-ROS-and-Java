188b24e0a58cb3680b88bf2e103804e4f920f671
Cryoza <EvilDoogTM@Gmail.com>
2022-11-09 15:10:28 +0200

    Adding another test, fixing master service

--- Machine system/machine_ws/src/autogator/nodes/master.py
+++ Machine system/machine_ws/src/autogator/nodes/master.py
@@ -7,15 +7,7 @@ import rospy
 
 from autogator.msg import location
 from autogator.services.masterService import MasterService
-
-
-def commandNet(req):
-    print("Returning [%s , %s , %s]" % (req.a, req.b, req.t))
-    return CommandNetResponse(req.a + req.b)
-
-
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "Data Received %s", data.data)
+from autogator.utils.autogator_worker import AutogatorWorker
 
 
 class master:
@@ -24,15 +16,18 @@ class master:
         rospy.init_node('master', anonymous=False)
 
         # Callback should be in a service itself
-        rospy.Subscriber("gps_location", location, MasterService.prepare_coordinates())
+        rospy.Subscriber("gps_location", location, MasterService.prepare_coordinates(location))
         rospy.logininfo("Master Started.")
 
         # Thread for publishing gps_track
-        pub = rospy.Publisher('gps_track', os.path.basename('../utils/models/gpsTrack.py'), queue_size=10)
+        # Maybe update with another method
+        publish_coord = AutogatorWorker("gps_location", function=MasterService.prepare_coordinates(location))
+        publish_coord.start()
 
-        service_s = rospy.Service('CommandNet', CommandNet, commandNet)
+        # Handling of the service inside the service class
+        service_s = rospy.Service('CommandNet', CommandNet, function=MasterService.handle_gpsTrack())
 
-        print(service_s)
+        # print(service_s)
 
         rospy.spin()
 
--- Machine system/machine_ws/src/autogator/src/autogator/services/masterService.py
+++ Machine system/machine_ws/src/autogator/src/autogator/services/masterService.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import rospy
-from autogator.msg import location
+from autogator.msg import location , gps_track
 from autogator.models.location import Location
+
 
 class MasterService:
 
@@ -10,12 +11,46 @@ class MasterService:
 
     @staticmethod
     def prepare_coordinates(location_):
-        # This is here to display better separation of concerns as of now.
-        # Should be updated with proper methods
-        # prepare the coords here
-        longitude = Location(location_.longitude)
-        latitude = Location(location_.latitude)
-        rotation = Location(location_.rotation)
-        rospy.logininfo("Coordinates: \nlong: %d,\nlat: %d,\nrot: %d", longitude, latitude, rotation)
+        # prepare the cords for gpsTrack here
+        rate = rospy.Rate(0.1)
 
+        if location_ is not None:
+            longitude = Location(location_.longitude)
+            latitude = Location(location_.latitude)
+            rotation = Location(location_.rotation)
+            rospy.logininfo("Coordinates: \nlong: %d,\nlat: %d,\nrot: %d", longitude, latitude, rotation)
 
+            gps_track_msg = gps_track()
+            record_no = 1
+            gps_track_msg.name = "Track" + str(int(record_no+1))
+
+            loc_msg = location()
+            loc_msg.longitude = longitude
+            loc_msg.latitude = latitude
+            loc_msg.rotation = rotation
+
+            for x in range(9):
+                gps_track_msg.points[x] = loc_msg
+                pass
+
+                if gps_track_msg is not None:
+                    MasterService.send_gpsTrack_to_master(gps_track_msg)
+        else:
+            rospy.logininfo("Something went wrong")
+        rate.sleep()
+
+    @staticmethod
+    def handle_gpsTrack(req):
+        # Handling of service
+        print("Returning [%s , %s , %s]" % (req.a, req.b, req.t))
+        resp = (req.a + req.b)
+        return resp
+
+        pass
+
+    @classmethod
+    def send_gpsTrack_to_master(cls, CommandNet):
+        pub = rospy.Publisher('gps_track', CommandNet, queue_size=10)
+        pub.publish(CommandNet)
+
+    pass
