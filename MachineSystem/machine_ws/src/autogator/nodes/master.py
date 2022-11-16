9c068512e3ccd812c8bd64cf52198857964bafc0
runnerking <293649@viauc.dk>
2022-11-16 11:12:01 +0100

    wip with master and testing stuff

--- MachineSystem/machine_ws/src/autogator/nodes/master.py
+++ MachineSystem/machine_ws/src/autogator/nodes/master.py
@@ -2,9 +2,8 @@
 from __future__ import print_function
 import rospy
 
-from autogator.msg import location, cmd_req
+from autogator.msg import Location, CmdReq
 from autogator.services.masterService import MasterService
-from autogator.utils.autogator_worker import AutogatorWorker
 
 
 class master:
@@ -14,13 +13,11 @@ class master:
 
         # Callback should be in a service itself
         self.master_service = MasterService()
-        rospy.Subscriber("location", location, self.master_service.handle_gps_track())
-        rospy.Subscriber("command", cmd_req, self.master_service.handle_command)
+        rospy.Subscriber("gps_location", Location, self.master_service.handle_new_gps_location)
+        rospy.Subscriber("command", CmdReq, self.master_service.handle_command)
         rospy.loginfo("Master Started.")
         # Thread for publishing gps_track
         # Maybe update with another method
-        publish_coord = AutogatorWorker("gps_location", function=self.master_service.handle_gps_track())
-        publish_coord.start()
 
         rospy.spin()
 
--- MachineSystem/machine_ws/src/autogator/src/autogator/services/masterService.py
+++ MachineSystem/machine_ws/src/autogator/src/autogator/services/masterService.py
@@ -1,113 +1,75 @@
 #!/usr/bin/env python
 import rospy
-from autogator.msg import location, gps_track, cmd_req
-from autogator.models.location import Location
+from autogator.msg import Location, GpsTrackMsg, CmdReq
+from autogator.models.gpsTrack import GpsTrack, GpsPoint
 
 
 class MasterService:
 
     def __init__(self):
+        self.tracking = False
+        self.track = GpsTrack()
+        self.seq = 1
+        self.loc_pub = rospy.Publisher('location', Location, queue_size=10)
+        self.track_pub = rospy.Publisher('gps_track', GpsTrackMsg, queue_size=10)
 
-        pass
-
-    @staticmethod
-    def handle_gps_track():
-        # Handling of service
-        # print("Returning [%s , %s , %s]" % (req.a, req.b, req.t))
-        # resp = (req.a + req.b)
-        # return resp
-        rate = rospy.Rate(0.1)
-
-        if location is not None:
-            longitude = Location(location.longitude)
-            latitude = Location(location.latitude)
-            rotation = Location(location.rotation)
-            rospy.loginfo("Coordinates: \nlong: %d,\nlat: %d,\nrot: %d", longitude, latitude, rotation)
-
-            gps_track_msg = gps_track()
-            record_no = 1
-            gps_track_msg.name = "Track" + str(int(record_no + 1))
-
-            loc_msg = location()
-            loc_msg.longitude = longitude
-            loc_msg.latitude = latitude
-            loc_msg.rotation = rotation
-
-            MasterService.send_gps_track_to_networking(gps_track_msg)
-        else:
-            rospy.loginfo("Something went wrong")
-        rate.sleep()
-
-    @staticmethod
-    def handle_new_point_to_route(condition):
+    def handle_new_point_to_route(self, location):
         # handle message from gps node on new location and add it to recording path
-        while condition:
-            if location is not None:
-                longitude = Location(location.longitude)
-                latitude = Location(location.latitude)
-                rotation = Location(location.rotation)
-                rospy.loginfo("Coordinates: \nlong: %d,\nlat: %d,\nrot: %d", longitude, latitude, rotation)
+        if location is not None:
+            point = GpsPoint(location.longitude, location.latitude)
+            self.track.add_track_point(point)
 
-                gps_track_msg = gps_track()
-                record_no = 1
-                gps_track_msg.name = "Track" + str(int(record_no + 1))
-
-                loc_msg = location()
-                loc_msg.longitude = longitude
-                loc_msg.latitude = latitude
-                loc_msg.rotation = rotation
-
-                if gps_track_msg is not None:
-                    MasterService.send_gps_track_to_networking(gps_track_msg)
+    def handle_start_recording_route(self):
+        # set global variable to true
+        self.tracking = True
+        # add name here.
+        self.track = GpsTrack("track" + str(self.seq))
+        self.seq += 1
         pass
 
-    @staticmethod
-    def handle_start_recording_route(command):
-        condition_ = bool(True)
-        if command is not None:
-            if cmd_req.type == "START_REC":
-
-                MasterService.handle_new_point_to_route(condition_)
-            elif cmd_req.type == "STOP_REC":
-                condition_ = bool(False)
-
-        else:
-            rospy.loginfo("Something went wrong")
-
-        pass
-
-    @staticmethod
-    def handle_command(command):
+    def handle_command(self, command):
         # handle cmd here and pass to proper func by type.
         # see what the boolean is
-        command_type = cmd_req.type
+        command_type = command.type
         if command_type == "START_REC":
-            rospy.loginfo("Current state : %s ,\n Success : %s", cmd_req.state, cmd_req.success)
-            MasterService.handle_start_recording_route(cmd_req)
+            rospy.loginfo("Current state : %s ,\n Success : %s", command.state, command.success)
+            self.handle_start_recording_route()
         elif command_type == "STOP_REC":
             # handle stop recording in diff func.
-            rospy.loginfo("Current state : %s ,\n Success : %s", cmd_req.state, cmd_req.success)
+            rospy.loginfo("Current state : %s ,\n Success : %s", command.state, command.success)
+            self.handle_stop_recording_route()
         elif command_type == "START_SELFDRIVE":
             # handle start self driving in diff func.
-            rospy.loginfo("Current state : %s ,\n Success : %s", cmd_req.state, cmd_req.success)
+            rospy.loginfo("Current state : %s ,\n Success : %s", command.state, command.success)
         elif command_type == "EM_STOP":
             # handle emergency stop in diff func.
-            rospy.loginfo("Current state : %s ,\n Success : %s", cmd_req.state, cmd_req.success)
+            rospy.loginfo("Current state : %s ,\n Success : %s", command.state, command.success)
         else:
             rospy.loginfo("Invalid command")
 
     pass
 
-    @classmethod
-    def send_gps_track_to_networking(cls, location):
-        pub = rospy.Publisher('location', location, queue_size=10)
-        pub.publish(location)
+    def send_gps_track_to_networking(self, track_msg):
+        self.track_pub.publish(track_msg)
 
     pass
 
-    @classmethod
-    def send_command_to_master(cls, cmd_req_):
-        pub = rospy.Publisher('command_req', cmd_req_, queue_size=10)
-        pub.publish(cmd_req_)
+    def handle_new_gps_location(self, location):
+        # handle message from gps node on new location and add it to recording path
+        rospy.loginfo("Coordinates: \nlong: %d,\nlat: %d,\nrot: %d", location.longitude, location.latitude,
+                      location.rotation)
+        if self.tracking:
+            MasterService.handle_new_point_to_route(self, location)
 
-    pass
+        # report to networking about location
+        self.loc_pub.publish(location)
+
+        pass
+
+    def handle_stop_recording_route(self):
+        self.tracking = False
+        # send track to networking
+        # map track to msg here
+
+        MasterService.send_gps_track_to_networking(self.track)
+        pass
