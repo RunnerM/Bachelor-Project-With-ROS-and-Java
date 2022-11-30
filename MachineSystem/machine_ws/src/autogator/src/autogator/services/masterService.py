d078734b4bc4c3510c89a2bc16b146ef6076f206
runnerking <293649@viauc.dk>
2022-11-30 09:29:25 +0100

    master delegation

--- MachineSystem/machine_ws/src/autogator/src/autogator/services/masterService.py
+++ MachineSystem/machine_ws/src/autogator/src/autogator/services/masterService.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from autogator.msg import Location, GpsTrackMsg, CmdReq
+from autogator.msg import Location, GpsTrackMsg, CmdReq, StartIrrigationRequest
 from autogator.models.gpsTrack import GpsTrack, GpsPoint
 
 
@@ -12,6 +12,7 @@ class MasterService:
         self.seq = 1
         self.loc_pub = rospy.Publisher('location', Location, queue_size=10)
         self.track_pub = rospy.Publisher('gps_track', GpsTrackMsg, queue_size=10)
+        self.irrigation_pub = rospy.Publisher('irrigation_command', StartIrrigationRequest, queue_size=10)
 
     def handle_new_point_to_route(self, location):
         # handle message from gps node on new location and add it to recording path
@@ -40,6 +41,11 @@ class MasterService:
             self.handle_stop_recording_route()
         elif command_type == "START_SELFDRIVE":
             # handle start self driving in diff func.
+            start_irrigation_req = StartIrrigationRequest()
+            start_irrigation_req.header.stamp = rospy.Time.now()
+            start_irrigation_req.header.frame_id = "location"
+            start_irrigation_req.path = command.path
+            self.irrigation_pub.publish(start_irrigation_req)
             rospy.loginfo("Current state : %s ,\n Success : %s", command.state, command.success)
         elif command_type == "EM_STOP":
             # handle emergency stop in diff func.
@@ -59,7 +65,7 @@ class MasterService:
         rospy.loginfo("Coordinates: \nlong: %d,\nlat: %d,\nrot: %d", location.longitude, location.latitude,
                       location.rotation)
         if self.tracking:
-            MasterService.handle_new_point_to_route(self, location)
+            self.handle_new_point_to_route(location)
 
         # report to networking about location
         self.loc_pub.publish(location)
